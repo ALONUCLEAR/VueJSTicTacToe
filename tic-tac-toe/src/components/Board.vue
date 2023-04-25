d3f0946285a8e8f5be52c6b4cf7a6446794aab3e
ALONUCLEAR <alonrazka58@gmail.com>
2023-04-26 01:55:01 +0300

    fix(Board.vue): start working on the board. Not close to done

--- tic-tac-toe/src/components/Board.vue
+++ tic-tac-toe/src/components/Board.vue
@@ -1,15 +1,42 @@
 <template>
   <div class="board">
-    
+    <div v-for="(row, index) in cells" :key="index">
+      <i v-for="(cell, colIndex) in row" :key="colIndex" class="fa-solid square">
+        {{cell}}
+      </i>
+    </div>    
   </div>
 </template>
 
 <script>
 export default {
   name: 'GameBoard',
+  props: {
+    rows: {
+      type: Number,
+      default: 3,
+    },
+    columns: {
+      type: Number,
+      default: 3,
+    }
+  },
+  data() {
+    return {
+      cells: [],
+    }
+  },
+  created() {
+    this.cells = Array.from({length: this.rows}).map(() => Array.from({length: this.columns}, () => 'x'));
+    console.log(this.cells);
+  }
 }
 </script>
 
 <style scoped>
-
+.square {
+  border: black;
+  outline-color: black;
+  outline-width: thick;
+}
 </style>
