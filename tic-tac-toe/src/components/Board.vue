84a70457bf5ad67be6170f69a57515d4fa1de252
ALONUCLEAR <alonrazka58@gmail.com>
2023-04-26 11:40:42 +0300

    feat(Board.vue): add methods to check if a game is over and actions correspoding to each game ending cause

--- tic-tac-toe/src/components/Board.vue
+++ tic-tac-toe/src/components/Board.vue
@@ -31,22 +31,100 @@ export default {
   },
   created() {
     this.cells = Array.from({length: this.rows}).map(() => Array.from({length: this.columns}, () => '')); 
-    console.log(this.cells);
   },
   methods: {
     error(text, title="Error!") {
       swal.fire({icon: "error", title, text})
     },
+    win(title, text) {
+      swal.fire({icon: "success", title, text})
+    },
+    draw() {
+      swal.fire({icon: "info", title: "Draw!", text: "No winners, no losers."})
+    },
     clickCell(row, col) {
-      if(this.cells[row][col].trim() !== "") {
+      if(this.didGameEnd()) {
+        this.error("The game has ended, If you want to play another game, press reset.", "Hold on!");
+      }
+      else if(this.cells[row][col].trim() !== "") {
         this.error("Cell is already clicked, try another one.");  
       } else {
         this.cells[row][col] = this.isXTurn ? this.x : this.circle;
-        console.log(this.cells[row][col]);
         this.isXTurn = !this.isXTurn;
         this.$forceUpdate();
+        this.announceEnd();
       }
-    }
+    },
+    announceEnd() {
+      if(this.didShapeWin('x')) {
+        this.win("X won!", "Good job x. You won the game!");
+      } else if(this.didShapeWin('o')) {
+        this.win("O won!", "Good job o. You won the game!");
+      } else if(this.isDraw()) {
+        this.draw();
+      }
+    },
+    isDraw() {
+      return this.cells.every(row => row.every(cell => cell !== ''));
+    },
+    didShapeWin(shape) {
+      const board = this.cells.map(row => row.map(cell => {
+        if(cell === this.x) {
+          return 'x'
+        }
+
+        return cell === this.circle ? 'o' : '';
+      }));
+
+      for (let rowIndex = 0; rowIndex < this.rows; rowIndex++) {
+        let count = 0;
+        for (let columnIndex = 0; columnIndex < this.columns; columnIndex++) {
+          if (board[rowIndex][columnIndex] === shape) {
+            count++;
+          }
+        }
+        if (count === this.columns) {
+          return true;
+        }
+      }
+
+      for (let columnIndex = 0; columnIndex < this.columns; columnIndex++) {
+        let count = 0;
+        for (let rowIndex = 0; rowIndex < this.rows; rowIndex++) {
+          if (board[rowIndex][columnIndex] === shape) {
+            count++;
+          }
+        }
+        if (count === this.rows) {
+          return true;
+        }
+      }
+
+      let count = 0;
+      for (let rowIndex = 0; rowIndex < this.rows; rowIndex++) {
+        if (board[rowIndex][rowIndex] === shape) {
+          count++;
+        }
+      }
+      if (count === this.rows) {
+        return true;
+      }
+
+      count = 0;
+      for (let rowIndex = 0; rowIndex < this.rows; rowIndex++) {
+        if (board[rowIndex][this.columns - 1 - rowIndex] === shape) {
+          count++;
+        }
+      }
+      if (count === this.rows) {
+        return true;
+      }
+
+      return false;
+    },
+    didGameEnd() {
+      return this.didShapeWin('x') || this.didShapeWin('o') || this.isDraw();
+    },
   },
 }
 </script>
